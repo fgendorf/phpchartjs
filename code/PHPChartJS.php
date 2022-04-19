97b2053c199a8be128bd20ca0358d83d2dd3c285
Frederico Gendorf <fred@esss.co>
2022-04-19 16:15:54 -0300

    add function to write headers inside a html

--- code/PHPChartJS.php
+++ code/PHPChartJS.php
@@ -2,28 +2,33 @@
 
 class PHPChartJS {
 
-    
     /**
      * PATH of relative place of vendor location
      * @var string 
      */
     protected $path;
 
-        /**
+    /**
      * value to be changed at each new release
      * @var string 
      */
     protected $sessionload;
 
     public function __construct() {
-        $this->sessionload = "2019052401";
+        $this->sessionload = "2022041901";
     }
 
-    public function setPath($path){
+    public function setPath($path) {
         $this->path = $path;
     }
 
     public function writeHeaders() {
         echo "<script type = \"text/javascript\" src = \"{$this->path}vendor/fgendorf/phpchartjs/Chart.bundle.min.js?s={$this->sessionload}\"></script>";
     }
+
+    public function writeInsideHeaders() {
+        $file = file_get_contents("{$this->path}/vendor/fgendorf/phpchartjs/Chart.bundle.min.js");
+        echo "<script>{$file}</script>";
+    }
+
 }
